b49c0d223bd0bbf7526ee681cf9609214a762e17
allencharlie838-pixel <allencharlie838@gmail.com>
2026-06-05 13:28:45 -0500

    Refactor loader.lua to use configurable repo and branch

--- loader.lua
+++ loader.lua
@@ -1,9 +1,13 @@
+local REPO = 'allencharlie838-pixel/skidv7'
+local BRANCH = 'main'
+
 local isfile = isfile or function(file)
 	local suc, res = pcall(function()
 		return readfile(file)
 	end)
 	return suc and res ~= nil and res ~= ''
 end
+
 local delfile = delfile or function(file)
 	writefile(file, '')
 end
@@ -11,23 +15,29 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/poopparty/poopparty/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/' .. REPO .. '/' .. readfile('newvape/profiles/commit.txt') .. '/' .. select(1, path:gsub('newvape/', '')), true)
 		end)
+
 		if not suc or res == '404: Not Found' then
 			error(res)
 		end
+
 		if path:find('.lua') then
-			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
+			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n' .. res
 		end
+
 		writefile(path, res)
 	end
+
 	return (func or readfile)(path)
 end
 
 local function wipeFolder(path)
 	if not isfolder(path) then return end
+
 	for _, file in listfiles(path) do
 		if file:find('loader') then continue end
+
 		if isfile(file) and select(1, readfile(file):find('--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.')) == 1 then
 			delfile(file)
 		end
@@ -41,21 +51,24 @@ for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/asset
 end
 
 if not shared.VapeDeveloper then
-	local _, subbed = pcall(function() 
-		return game:HttpGet('https://github.com/poopparty/poopparty') 
+	local _, subbed = pcall(function()
+		return game:HttpGet('https://github.com/' .. REPO)
 	end)
-	local commit = subbed:find('currentOid')
+
+	local commit = subbed and subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
-	commit = commit and #commit == 40 and commit or 'main'
-	if commit == 'main' or (isfile('newvape/profiles/commit.txt') and readfile('newvape/profiles/commit.txt') or '') ~= commit then
+	commit = commit and #commit == 40 and commit or BRANCH
+
+	if commit == BRANCH or (isfile('newvape/profiles/commit.txt') and readfile('newvape/profiles/commit.txt') or '') ~= commit then
 		wipeFolder('newvape')
 		wipeFolder('newvape/games')
 		wipeFolder('newvape/guis')
 		wipeFolder('newvape/libraries')
 	end
+
 	writefile('newvape/profiles/commit.txt', commit)
 end
 
 return loadstring(downloadFile('newvape/main.lua'), 'main')({
-    Username = shared.ValidatedUsername
+	Username = shared.ValidatedUsername
 })
